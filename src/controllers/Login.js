851030646c96b654b83e8f5386f52ea1e2c9965a
nairoMudumane <nazirmudumane@gmail.com>
2021-11-29 11:20:30 +0200

    fixed login controller

--- src/controllers/Login.js
+++ src/controllers/Login.js
@@ -1,61 +1,67 @@
-const conn = require('../database/connection').conn
+const conn = require('../database/connection').conn;
 
-exports.loginStudent = async(req, res) => {
-    const requestData = req.body
-    const list = []
-    const data = []
+exports.loginStudent = (req, res) => {
+  const requestData = req.body;
+  const list = [];
+  const data = [];
 
-    await conn.query(`SELECT * FROM students`, (err, rows, fields) => {
-        if (err) {
-            return res
-                .status(400)
-                .json({ error: true, message: 'Erro na conexao a base de dados' })
-        }
+  conn.query(`SELECT * FROM students`, (err, rows, fields) => {
+    if (err) {
+      return res
+        .status(400)
+        .json({ error: true, message: 'Failed to fetch database' });
+    }
 
-        rows.forEach((student) => {
-            list.push(student)
-        })
+    rows.forEach((student) => {
+      list.push(student);
+    });
 
-        list.forEach((student) => {
-            if (student.id_student == requestData.id_student) {
-                data.push(student)
-            }
-        })
-        if (data == []) {
-            return res
-                .status(400)
-                .json({ error: true, message: 'Usuario inexistente' })
-        }
-        return res.status(200).json({ error: false, data: data })
-    })
-}
+    list.forEach((student) => {
+      if (
+        requestData.email == student.email &&
+        requestData.password == student.password
+      ) {
+        data.push(student);
+      }
+    });
 
-exports.loginAdmin = async(req, res) => {
-    const requestData = req.body
-    const list = []
-    const data = []
+    if (data.length == 0) {
+      return res.status(404).json({ error: true, message: 'Not found' });
+    } else {
+      return res.status(200).json({ error: false, data: data });
+    }
+  });
+};
 
-    await conn.query(`SELECT * FROM admin`, (err, rows, fields) => {
-        if (err) {
-            return res
-                .status(400)
-                .json({ error: true, message: 'Erro na conexao a base de dados' })
-        }
+exports.loginAdmin = (req, res) => {
+  const requestData = req.body;
+  const list = [];
+  const data = [];
 
-        rows.forEach((admin) => {
-            list.push(admin)
-        })
+  conn.query(`SELECT * FROM admin`, (err, rows, fields) => {
+    if (err) {
+      return res
+        .status(400)
+        .json({ error: true, message: 'Failed to fetch database' });
+    }
 
-        list.forEach((admin) => {
-            if (admin.id_admin == requestData.id_admin) {
-                data.push(admin)
-            }
-        })
-        if (data == []) {
-            return res
-                .status(400)
-                .json({ error: true, message: 'Usuario inexistente' })
-        }
-        return res.status(200).json({ error: false, data: data })
-    })
-}
+    rows.forEach((admin) => {
+      list.push(admin);
+    });
+
+    list.forEach((admin) => {
+      if (
+        requestData.email == admin.email &&
+        requestData.password == admin.password
+      ) {
+        data.push(admin);
+      }
+    });
+
+    if (data.length == 0) {
+      return res.status(404).json({ error: true, message: 'Not found' });
+    } else {
+      return res.status(200).json({ error: false, data: data });
+    }
+  });
+};
