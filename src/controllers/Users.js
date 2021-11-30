05bea652f3f908d313281ae52aefddb33600319d
nairoMudumane <nazirmudumane@gmail.com>
2021-11-30 04:27:47 +0200

    formating code

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,80 +1,81 @@
-const conn = require('../database/connection').conn
+const conn = require('../database/connection').conn;
 
-exports.newStudent = async(req, res) => {
-    const requestData = req.body
+exports.newStudent = async (req, res) => {
+  const requestData = req.body;
 
-    conn.query(
-        'INSERT INTO students SET ?',
-        requestData,
-        (err, results, fields) => {
-            if (err) {
-                console.log(err)
-                return res.status(400).json({ error: true, message: 'Not created' })
-            }
-            console.log(results)
-            return res.status(201).json({ error: false, data: 'Created' })
-        },
-    )
-}
+  conn.query(
+    'INSERT INTO students SET ?',
+    requestData,
+    (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ error: true, message: 'Not created' });
+      }
+      console.log(results);
+      return res.status(201).json({ error: false, data: 'Created' });
+    }
+  );
+};
 
-exports.updateStudent = async(req, res) => {
-    const requestData = req.body
-    const sql =
-        'UPDATE students SET name = ? , type_document = ? WHERE id_student = ?'
-    conn.query(
-        sql, [requestData.name, requestData.type_document, requestData.id_student],
-        (err, result) => {
-            if (err) {
-                return res
-                    .status(400)
-                    .json({ error: true, message: 'Failed on update' })
-            }
-            return res.status(200).json({ error: false, data: 'Updated' })
-        },
-    )
-}
+exports.updateStudent = async (req, res) => {
+  const requestData = req.body;
+  const sql =
+    'UPDATE students SET name = ? , type_document = ? WHERE id_student = ?';
+  conn.query(
+    sql,
+    [requestData.name, requestData.type_document, requestData.id_student],
+    (err, result) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ error: true, message: 'Failed on update' });
+      }
+      return res.status(200).json({ error: false, data: 'Updated' });
+    }
+  );
+};
 
-exports.readStudents = async(req, res) => {
-    const list = []
+exports.readStudents = async (req, res) => {
+  const list = [];
 
-    await conn.query('SELECT * FROM students', (err, rows, fields) => {
-        if (err) {
-            return res
-                .status(400)
-                .json({ error: true, message: 'Failed to fetch data' })
-        }
+  await conn.query('SELECT * FROM students', (err, rows, fields) => {
+    if (err) {
+      return res
+        .status(400)
+        .json({ error: true, message: 'Failed to fetch data' });
+    }
 
-        rows.forEach((student) => {
-            list.push(student)
-        })
+    rows.forEach((student) => {
+      list.push(student);
+    });
 
-        return res.status(200).json({ error: false, data: list })
-    })
-}
+    return res.status(200).json({ error: false, data: list });
+  });
+};
 
-exports.readById = async(req, res) => {
-    const { id_student } = req.params
-    const list = []
+exports.readById = async (req, res) => {
+  const { id_student } = req.params;
+  const list = [];
 
-    await conn.query(
-        `SELECT * FROM students WHERE id_student = ${id_student}`,
-        (err, rows, fields) => {
-            if (err) {
-                console.log(err)
-                return res
-                    .status(400)
-                    .json({ error: true, message: 'Failed to fetch data' })
-            }
+  await conn.query(
+    `SELECT * FROM students WHERE id_student = ${id_student}`,
+    (err, rows, fields) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ error: true, message: 'Failed to fetch data' });
+      }
 
-            rows.forEach((student) => {
-                list.push(student)
-            })
+      rows.forEach((student) => {
+        list.push(student);
+      });
 
-            if (list == []) {
-                return res.status(404).json({ error: true, message: 'Not found' })
-            }
+      if (list == []) {
+        return res.status(404).json({ error: true, message: 'Not found' });
+      }
 
-            return res.status(200).json({ error: false, data: list })
-        },
-    )
-}
+      return res.status(200).json({ error: false, data: list });
+    }
+  );
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,22 @@
-const Login = require('../controllers/Login')
-const Users = require('../controllers/Users')
+const Login = require('../controllers/Login');
+const Users = require('../controllers/Users');
 
 module.exports = (app) => {
-    // users
-    app.post('/new', Users.newStudent)
-    app.post('/update/student', Users.updateStudent)
-    app.get('/students', Users.readStudents)
-    app.get('/students/:id_student', Users.readById)
+  // users
+  app.post('/new', Users.newStudent);
+  app.post('/update/student', Users.updateStudent);
+  app.get('/students', Users.readStudents);
+  app.get('/students/:id_student', Users.readById);
 
-    // login
-    app.post('/login', Login.loginStudent)
-    app.post('/admin/login', Login.loginAdmin)
+  // login
+  app.post('/login', Login.loginStudent);
+  app.post('/admin/login', Login.loginAdmin);
 
-    // no response
-    app.get('/', (req, res) => {
-        return res.status(400).send('<p>No response!</p>')
-    })
-    app.get('/*', (req, res) => {
-        return res.redirect('/')
-    })
-}
+  // no response
+  app.get('/', (req, res) => {
+    return res.status(400).send('<p>No response!</p>');
+  });
+  app.get('/*', (req, res) => {
+    return res.redirect('/');
+  });
+};
